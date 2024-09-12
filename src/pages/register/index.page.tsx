f9e36ce8a5a028d931a6699545babb12545ce62b
João Alberto Redondo <169093784+joaoalbertoredondo@users.noreply.github.com>
2024-09-12 13:35:25 -0300

    formulário de cadastro

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,8 +1,38 @@
 import { Button, Heading, MultiStep, Text, TextInput } from "@ignite-ui/react";
-import { Container, Form, Header } from "./styles";
+import { Container, Form, FormError, Header } from "./styles";
 import { ArrowRight } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const registerFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: "O usuário precisa ter pelo menos 3 letras." })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: "o usuário pode ter apenas letras e hifens.",
+    })
+    .transform((username) => username.toLocaleLowerCase()),
+  name: z
+    .string()
+    .min(3, { message: "O nome precisa ter pelo menos 3 letras." }),
+});
+
+type RegisterFormData = z.infer<typeof registerFormSchema>;
 
 export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<RegisterFormData>({
+    resolver: zodResolver(registerFormSchema),
+  });
+
+  async function handleRegister(data: RegisterFormData) {
+    console.log(data);
+  }
+
   return (
     <Container>
       <Header>
@@ -15,7 +45,7 @@ export default function Register() {
         <MultiStep size={4} currentStep={1} />
       </Header>
 
-      <Form as="form">
+      <Form as="form" onSubmit={handleSubmit(handleRegister)}>
         <label>
           <Text size="sm">Nome do usuário</Text>
           <TextInput
@@ -24,7 +54,12 @@ export default function Register() {
             crossOrigin=""
             onPointerEnterCapture=""
             onPointerLeaveCapture=""
+            {...register("username")}
           />
+
+          {errors.username && (
+            <FormError size={"sm"}>{errors.username.message}</FormError>
+          )}
         </label>
 
         <label>
@@ -34,11 +69,16 @@ export default function Register() {
             crossOrigin=""
             onPointerEnterCapture=""
             onPointerLeaveCapture=""
+            {...register("name")}
           />
+
+          {errors.name && (
+            <FormError size={"sm"}>{errors.name.message}</FormError>
+          )}
         </label>
 
-        <Button type="submit">
-          Próximo paaso
+        <Button type="submit" disabled={isSubmitting}>
+          Próximo passo
           <ArrowRight />
         </Button>
       </Form>
